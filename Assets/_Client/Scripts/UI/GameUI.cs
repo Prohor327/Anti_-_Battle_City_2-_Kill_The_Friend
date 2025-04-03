c91496e282a0e396bf14f34c9e90de2ffe99fc64
Ya_VaFel <ilyamensikov77@gmail.com>
2025-04-03 23:42:31 +0500

    003

--- Assets/_Client/Scripts/UI/GameUI.cs
+++ Assets/_Client/Scripts/UI/GameUI.cs
@@ -7,6 +7,9 @@ public class GameUI : UIElement
     private VisualElement _expPlayer2;
     private VisualElement _hpPlayer1;
     private VisualElement _hpPlayer2;
+    private Label _scorePlayer1;
+    private Label _scorePlayer2;
+    private Label _timer;
 
     protected override void Initialize()
     {
@@ -16,6 +19,9 @@ public class GameUI : UIElement
         _expPlayer2 = _UIElement.Q<VisualElement>("ExperiencePlayer2");
         _hpPlayer1 = _UIElement.Q<VisualElement>("HPPlayer1");
         _hpPlayer2 = _UIElement.Q<VisualElement>("HPPlayer2");
+        _scorePlayer1 = _UIElement.Q<Label>("ScorePlayer1");
+        _scorePlayer1 = _UIElement.Q<Label>("ScorePlayer2");
+        _timer = _UIElement.Q<Label>("Timer");
 
         Game.Instance.gameMachine.OnLoadedLevel += Open;
     }
@@ -25,8 +31,40 @@ public class GameUI : UIElement
         base.Open();
     }
 
-    private void UpdateState(VisualElement _element, float param)
+    private void UpdateHP(int playerIndex, float param)
     {
-        _element.style.width = param;
+        if (playerIndex == 1)
+        {
+            _hpPlayer1.style.width = param;
+        }
+        else
+        {
+            _hpPlayer2.style.width = param;
+        }
     }
+
+    private void UpdateExp(int playerIndex, float param)
+    {
+        if (playerIndex == 1)
+        {
+            _expPlayer1.style.height = param;
+        }
+        else
+        {
+            _expPlayer2.style.height = param;
+        }
+    }
+
+    private void UpdateScore(int playerIndex, float param)
+    {
+        if (playerIndex == 1)
+        {
+            _scorePlayer1.text = param.ToString();
+        }
+        else
+        {
+            _scorePlayer2.text = param.ToString();
+        }
+    }
+
 }
