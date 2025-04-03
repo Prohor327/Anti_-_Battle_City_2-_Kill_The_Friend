53e966d90248ca2e4d9206c0e15730f665638ec0
Ya_VaFel <ilyamensikov77@gmail.com>
2025-04-04 00:46:56 +0500

    last

--- Assets/_Client/Scripts/UI/GameUI.cs
+++ Assets/_Client/Scripts/UI/GameUI.cs
@@ -3,10 +3,10 @@ using UnityEngine.UIElements;
 
 public class GameUI : UIElement
 {
-    private VisualElement _expPlayer1;
-    private VisualElement _expPlayer2;
-    private VisualElement _hpPlayer1;
-    private VisualElement _hpPlayer2;
+    private ProgressBar _expPlayer1;
+    private ProgressBar _expPlayer2;
+    private ProgressBar _hpPlayer1;
+    private ProgressBar _hpPlayer2;
     private Label _scorePlayer1;
     private Label _scorePlayer2;
     private Label _timer;
@@ -15,10 +15,10 @@ public class GameUI : UIElement
     {
         base.Initialize();
 
-        _expPlayer1 = _UIElement.Q<VisualElement>("ExperiencePlayer1");
-        _expPlayer2 = _UIElement.Q<VisualElement>("ExperiencePlayer2");
-        _hpPlayer1 = _UIElement.Q<VisualElement>("HPPlayer1");
-        _hpPlayer2 = _UIElement.Q<VisualElement>("HPPlayer2");
+        _expPlayer1 = _UIElement.Q<ProgressBar>("ExperiencePlayer1");
+        _expPlayer2 = _UIElement.Q<ProgressBar>("ExperiencePlayer2");
+        _hpPlayer1 = _UIElement.Q<ProgressBar>("HPPlayer1");
+        _hpPlayer2 = _UIElement.Q<ProgressBar>("HPPlayer2");
         _scorePlayer1 = _UIElement.Q<Label>("ScorePlayer1");
         _scorePlayer1 = _UIElement.Q<Label>("ScorePlayer2");
         _timer = _UIElement.Q<Label>("Timer");
